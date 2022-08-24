12724928a7904c37bb1e734894375b8745c0adb3
OhItsLena <lena.hammerer@vol.at>
2022-08-24 18:55:41 +0200

    switch to confluent_kafka and restructuring

--- Kafka.py
+++ Kafka.py
@@ -1,74 +1,90 @@
 import json
+from typing import Dict
 import uuid
+import os
+from dotenv import load_dotenv
+from confluent_kafka import SerializingProducer, DeserializingConsumer, TopicPartition
+from kafka3 import KafkaProducer, KafkaConsumer
 
-from kafka3 import KafkaProducer, KafkaConsumer, TopicPartition
+
+class KafkaConfluentWriter:
+    producer = None
+    topic = None
+
+    def __init__(self, topic) -> None:
+        load_dotenv()
+        self.producer = SerializingProducer({
+            'bootstrap.servers': os.getenv('BOOTSTRAPSERVERS'),
+            'key.serializer': self.keyencoder,
+            'value.serializer': self.valueencoder
+        })
+        self.topic = topic
+
+    def keyencoder(self, k, ctx) -> str:
+        return str.encode(k)
+
+    def valueencoder(self, v, ctx) -> json:
+        return json.dumps(v).encode('utf-8')
+
+    def delivery_report(self, err, msg) -> None:
+        """ Called once for each message produced to indicate delivery result.
+            Triggered by poll() or flush(). """
+        if err is not None:
+            print('Message delivery failed: {}'.format(err))
+        else:
+            print('Message delivered to {} [{}]'.format(
+                msg.topic(), msg.partition()))
+
+    def produce(self, key, msg) -> None:
+        self.producer.poll(0)
+        self.producer.produce(topic=self.topic, key=key,
+                              value=msg, on_delivery=self.delivery_report)
+        self.producer.flush()
 
 
-class Kafka:
-    #kafka_bootstrap_server = 'kafka-1:19092'
-    kafka_bootstrap_server = 'localhost:9092,localhost:9093,localhost:9094'
-    kafka_offset_reset = 'earliest'
-    kafka_topic = 'weather.forecast'
-    topic_partition = TopicPartition(kafka_topic, 0)
+class KafkaConfluentReader:
+    consumer = None
+    topic = None
 
-    producer: KafkaProducer
-    consumer: KafkaConsumer
+    def __init__(self, topic: str, autoCommit: bool) -> None:
+        load_dotenv()
+        self.topic = topic
+        self.consumer = DeserializingConsumer({
+            'bootstrap.servers': os.getenv('BOOTSTRAPSERVERS'),
+            'group.id': 'buffering',
+            'client.id': 'buffering',
+            'auto.offset.reset': 'earliest',
+            'enable.auto.commit': autoCommit,
+            'value.deserializer': self.valuedecoder,
+            'key.deserializer': self.keydecoder
+        })
+        self.consumer.assign([TopicPartition(self.topic, 0)])
 
-    def close(self):
-        self.producer.close()
+    def keydecoder(self, k, ctx):
+        return k.decode('utf-8')
+
+    def valuedecoder(self, v, ctx):
+        return json.loads(v.decode('utf-8'))
+
+    def poll(self, ms) -> object:
+        return self.consumer.poll(ms)
+
+    def get_latest_message(self, ms) -> object:
+        offsets = self.consumer.get_watermark_offsets(
+            TopicPartition(self.topic, 0))
+        self.consumer.seek(TopicPartition(
+            self.topic, 0, offsets[1]-1 if offsets[1] > 0 else 0))
+        return self.consumer.poll(ms)
+
+    def get_all_messages(self, ms) -> {}:
+        messages = {}
+        while True:
+            msg = self.consumer.poll(ms)
+            if msg is None:
+                break
+            else:
+                messages[msg.key()] = msg.value()
+        return messages
+
+    def close(self) -> None:
         self.consumer.close()
-
-
-class KafkaWriter(Kafka):
-    def __init__(self, kafka_topic: str):
-        try:
-            self.producer = KafkaProducer(
-                bootstrap_servers=self.kafka_bootstrap_server,
-                key_serializer=str.encode,
-                value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-            self.kafka_topic = kafka_topic
-
-        except ValueError as ve:
-            # might hapen when the bootstrap server is unreachable
-            print(f'  !!! error while creating kafka producer: {ve}')
-
-    def store(self, message_key: str, data: json) -> None:
-        if self.producer is not None:
-            self.producer.send(self.kafka_topic, key=message_key, value=data)
-            self.producer.flush()
-
-
-class KafkaReader(Kafka):
-    def __init__(self, kafka_topic: str):
-        try:
-            self.consumer = KafkaConsumer(
-                bootstrap_servers=self.kafka_bootstrap_server,
-                group_id=uuid.uuid4().hex,
-                client_id=uuid.uuid4().hex,
-                auto_offset_reset=self.kafka_offset_reset,
-                enable_auto_commit=True,
-
-                key_deserializer=lambda k: k.decode('utf-8'),
-                value_deserializer=lambda m: json.loads(m.decode('utf-8'))
-            )
-            self.kafka_topic = kafka_topic
-        except ValueError as ve:
-            # might hapen when the bootstrap server is unreachable
-            print(f'  !!! error while creating kafka consumer: {ve}')
-        if self.consumer is not None:
-            self.consumer.assign([self.topic_partition])
-
-    def retrieve(self) -> {}:
-        predictions = {}
-        if self.consumer is not None:
-            while True:  # poll until the size of returned messages is zero, then break the loop
-                messages = self.consumer.poll(timeout_ms=3000)
-                if len(messages) == 0:
-                    # no more messages in topic
-                    break
-
-                for key in messages.keys():
-                    records = messages[key]
-                    for record in records:
-                        predictions[record.key] = record.value
-        return predictions
